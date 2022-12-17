a70c3b984266ea56577d90c32b0996778e78c43e
Християн Христов <itco.official18@gmail.com>
2022-12-17 15:57:56 +0200

    _

---
+++ C# Advanced/C# ADVANCED/RegularExam/RegularExam/01._Energy_Drinks/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace _01._Energy_Drinks
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] mlCaffeine = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int[] energyDrinks = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            Stack<int> mlCaf = new Stack<int>(mlCaffeine);
+            Queue<int> enDrin = new Queue<int>(energyDrinks);
+
+            int totalCaffeine = 0;
+            int maxCaffeine = 300;
+            while (mlCaf.Count != 0 && enDrin.Count != 0)
+            { 
+                int currentDrank = mlCaf.Peek() * enDrin.Peek();
+                if(currentDrank + totalCaffeine <= maxCaffeine)
+                {
+                    mlCaf.Pop();
+                    enDrin.Dequeue();
+                    totalCaffeine += currentDrank;
+                    continue;
+                }
+                else if(currentDrank + totalCaffeine > maxCaffeine)
+                {
+                    mlCaf.Pop();
+                    enDrin.Enqueue(enDrin.Dequeue());
+                    totalCaffeine -= 30;
+                    if (totalCaffeine <0)
+                    {
+                        totalCaffeine = 0;
+                    }
+                    continue;
+                }
+            }
+
+            if (enDrin.Count!=0)
+            {
+                Console.WriteLine($"Drinks left: {string.Join(", ", enDrin)}");
+            }
+            else if(enDrin.Count==0)
+            {
+                Console.WriteLine($"At least Stamat wasn't exceeding the maximum caffeine.");
+            }
+                Console.WriteLine($"Stamat is going to sleep with {totalCaffeine} mg caffeine.");
+        }
+    }
+}
---
+++ C# Advanced/C# ADVANCED/RegularExam/RegularExam/02._Rally_Racing/Program.cs
@@ -0,0 +1,161 @@
+﻿namespace _02._Rally_Racing
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            char[,] matrix = new char[n, n];
+            int rNum = int.Parse(Console.ReadLine());
+            int km = 0;
+            int cRow = 0;
+            int cCol = 0;
+            string result = $"Racing car {rNum} DNF.";
+            List<int> finish = new List<int>();
+            List<int> tStart = new List<int>();
+            List<int> tEnd = new List<int>();
+
+            for (int row = 0; row < n; row++)
+            {
+                char[] cmd = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(char.Parse).ToArray();
+                for (int col = 0; col < n; col++)
+                {
+                    if(cmd[col] == 'F')
+                    {
+                        finish.Add(row);
+                        finish.Add(col);
+                    }
+                    else if (cmd[col]=='T' && tStart.Count==0)
+                    {
+                        tStart.Add(row);
+                        tStart.Add(col);
+                    }
+                    else if(cmd[col] == 'T' && tStart.Count != 0)
+                    {
+                        tEnd.Add(row);
+                        tEnd.Add(col);
+                    }
+                    matrix[row,col]= cmd[col];
+                }
+            }
+            while (true)
+            {
+                string cmd = Console.ReadLine();
+                switch(cmd.ToLower())
+                {
+                    case "left":
+                        cCol--;
+                        if(cRow == finish[0] && cCol == finish[1])
+                        {
+                            km += 10;
+                            matrix[finish[0], finish[1]] = 'C';
+                            result = $"Racing car {rNum} finished the stage!";
+                        }
+                        else if (cRow == tStart[0] && cCol == tStart[1])
+                        {
+                            km += 30;
+                            cRow = tEnd[0];
+                            cCol = tEnd[1];
+                            matrix[tStart[0], tStart[1]] = '.';
+                            matrix[tEnd[0], tEnd[1]] = '.';
+                        }
+                        else
+                        {
+                            km += 10;
+                        }
+                        break;
+                    case "right":
+                        cCol++;
+                        if (cRow == finish[0] && cCol == finish[1])
+                        {
+                            km += 10;
+                            matrix[finish[0], finish[1]] = 'C';
+                            result = $"Racing car {rNum} finished the stage!";
+                        }
+                        else if (cRow == tStart[0] && cCol == tStart[1])
+                        {
+                            km += 30;
+                            cRow = tEnd[0];
+                            cCol = tEnd[1];
+                            matrix[tStart[0], tStart[1]] = '.';
+                            matrix[tEnd[0], tEnd[1]] = '.';
+                        }
+                        else
+                        {
+                            km += 10;
+                        }
+                        break;
+                    case "up":
+                        cRow--;
+                        if (cRow == finish[0] && cCol == finish[1])
+                        {
+                            km += 10;
+                            matrix[finish[0], finish[1]] = 'C';
+                            result = $"Racing car {rNum} finished the stage!";
+                        }
+                        else if (cRow == tStart[0] && cCol == tStart[1])
+                        {
+                            km += 30;
+                            cRow = tEnd[0];
+                            cCol = tEnd[1];
+                            matrix[tStart[0], tStart[1]] = '.';
+                            matrix[tEnd[0], tEnd[1]] = '.';
+                        }
+                        else
+                        {
+                            km += 10;
+                        }
+                        break;
+                    case "down":
+                        cRow++;
+                        if (cRow == finish[0] && cCol == finish[1])
+                        {
+                            km += 10;
+                            matrix[finish[0], finish[1]] = 'C';
+                            result = $"Racing car {rNum} finished the stage!";
+                        }
+                        else if (cRow == tStart[0] && cCol == tStart[1])
+                        {
+                            km += 30;
+                            cRow = tEnd[0];
+                            cCol = tEnd[1];
+                            matrix[tStart[0], tStart[1]] = '.';
+                            matrix[tEnd[0], tEnd[1]] = '.';
+                        }
+                        else
+                        {
+                            km += 10;
+                        }
+                        break;
+                }
+
+                if (result!= $"Racing car {rNum} DNF.")
+                {
+                    Console.WriteLine(result);
+                    break;
+                }
+                else if (cmd.ToLower()=="end")
+                {
+                    matrix[cRow,cCol] = 'C';
+                    Console.WriteLine($"Racing car {rNum} DNF.");
+                    break;
+                }
+            }
+
+            Console.WriteLine($"Distance covered {km} km.");
+
+            for (int row = 0; row < n; row++)
+            {
+                for (int col = 0; col < n; col++)
+                {
+                    Console.Write(matrix[row,col]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
